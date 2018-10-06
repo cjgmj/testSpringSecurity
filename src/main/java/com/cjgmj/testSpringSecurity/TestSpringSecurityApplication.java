fa5b7d0127dcffc6be29905ba913ee83a931e6c8
cjgmj <cjgmj@hotmail.com>
2018-10-06 14:15:15 +0200

    Proyecto base

---
+++ src/main/java/com/cjgmj/testSpringSecurity/TestSpringSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.cjgmj.testSpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringSecurityApplication.class, args);
+	}
+}
